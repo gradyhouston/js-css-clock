75677a981629b006cac29ee84427ede2067c7d46
Grady <Gradyhouston@gmail.com>
2018-03-25 21:56:32 -0400

    adding completed project

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+//
+// Clock
+//
+// by Grady Houston
+
+
+var secondHand = document.querySelector(".second-hand");
+var minuteHand = document.querySelector(".min-hand");
+var hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  var now = new Date();
+
+  var seconds = now.getSeconds();
+  var secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(seconds);
+
+  var minutes = now.getMinutes();
+  var minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  console.log(minutes);
+
+  var hours = now.getHours();
+  var hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  console.log(hours);
+}
+
+setInterval(setDate, 1000);
